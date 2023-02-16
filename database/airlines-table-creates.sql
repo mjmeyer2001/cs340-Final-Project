d7d1b3e668c2458985cd52dbb7350d7029219576
mjmeyer2001 <mjmeyer2001@gmail.com>
2023-02-15 17:17:41 -0800

    set up web server

--- database/airlines-table-creates.sql
+++ database/airlines-table-creates.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS airlines;
 
 CREATE OR REPLACE TABLE airlines (
     airline_id varchar(3) UNIQUE NOT NULL,
-    name varchar(255),
+    name varchar(75),
     PRIMARY KEY (airline_id)
 );
 
@@ -13,8 +13,8 @@ DROP TABLE IF EXISTS airports;
 
 CREATE OR REPLACE TABLE airports (
 	airport_id varchar(3) UNIQUE NOT NULL,
-    name varchar(255),
-    location varchar(255),
+    name varchar(75),
+    location varchar(75),
     PRIMARY KEY (airport_id)
 );
 
@@ -24,7 +24,7 @@ CREATE OR REPLACE TABLE planes (
 	plane_id int AUTO_INCREMENT UNIQUE NOT NULL,
 	airline_id varchar(3),
     passenger_capacity int NOT NULL,
-    manufacturer varchar(255),
+    manufacturer varchar(75),
     PRIMARY KEY (plane_id),
     FOREIGN KEY (airline_id) REFERENCES airlines(airline_id) ON DELETE CASCADE
 );
@@ -34,17 +34,14 @@ DROP TABLE IF EXISTS flights;
 CREATE OR REPLACE TABLE flights (
 	flight_id int AUTO_INCREMENT UNIQUE NOT NULL,
 	plane_id int NOT NULL,
-    	# may not need airline_id (a plane can only belong to one airline, so is redundant)
-    	# airline_id varchar(3) NOT NULL,
-    	departure_time datetime,
-    	arrival_time datetime,
-    	origin varchar(255) NOT NULL,
-    	destination varchar(255) NOT NULL,
-    	PRIMARY KEY (flight_id),
-    	FOREIGN KEY (plane_id) REFERENCES planes(plane_id) ON DELETE CASCADE,
-    	#FOREIGN KEY (airline_id) REFERENCES airlines(airline_id) ON DELETE CASCADE,
-    	FOREIGN KEY (origin) REFERENCES airports(airport_id) ON DELETE CASCADE,
-    	FOREIGN KEY (destination) REFERENCES airports(airport_id) ON DELETE CASCADE
+    departure_time datetime,
+    arrival_time datetime, 
+    origin_airport_id varchar(3) NOT NULL,
+    destination_airport_id varchar(3) NOT NULL,
+    PRIMARY KEY (flight_id),
+    FOREIGN KEY (plane_id) REFERENCES planes(plane_id) ON DELETE CASCADE,
+    FOREIGN KEY (origin_airport_id) REFERENCES airports(airport_id) ON DELETE CASCADE,
+   	FOREIGN KEY (destination_airport_id) REFERENCES airports(airport_id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS airlines_has_airports;
